ba33001b9639db880fbe06670c2e796a06dd5bb0
Taran Pabla <tpabla@gmail.com>
2016-12-12 17:22:25 -0800

    Updates to allow jobs to be run immediately in tests, and readme updates

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,8 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "easy_stalk"
 
 require 'support/mock_beaneater'
+require 'support/immediate_job_runner'
+require 'support/job'
 
 RSpec.configure do |config|
   config.color = true
