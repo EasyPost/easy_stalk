8d5d9ce016826fa00da000e6e947f5c7a3eb7c1d
Joshua Lane <joshlane@easypost.com>
2021-08-17 14:15:11 -0700

    refactor: use same servers strategy for the producer

--- lib/easy_stalk/producer_pool.rb
+++ lib/easy_stalk/producer_pool.rb
@@ -5,11 +5,13 @@ class EasyStalk::ProducerPool
     @default ||= create
   end
 
-  def self.create(servers: EasyStalk.servers, size: EasyStalk.pool_size,
-                  timeout: EasyStalk.timeout_seconds)
-
+  def self.create(
+    servers: EasyStalk.servers.shuffle.to_enum.cycle,
+    size: EasyStalk.pool_size,
+    timeout: EasyStalk.timeout_seconds
+  )
     EzPool.new(size: size, timeout: timeout) do
-      EasyStalk::Client.new(Beaneater.new(servers.sample))
+      EasyStalk::Client.new(Beaneater.new(servers.next))
     end
   end
 end
